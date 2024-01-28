df0d12c2d0c7fb3a69b051030a8ac56788741530
satyam2319 <57935143+satyam2319@users.noreply.github.com>
2024-01-28 14:41:09 +0000

    new js file

---
+++ 01_basics/01_var.js
@@ -0,0 +1,10 @@
+const accountId = 141414
+var accountName = "Satyam";
+let accountCity = "Delhi";
+accountEmail = "satyam@g.com";
+
+let accountState;
+
+accountCity = "Kochi";
+
+console.table([accountId,accountCity,accountName,accountEmail,accountState]);
